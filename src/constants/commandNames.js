21b1be96218d3c64bb7bbb1293ea7d0e8c189bf7
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-21 16:44:32 +0300

    command names added

---
+++ src/constants/commandNames.js
@@ -0,0 +1,5 @@
+export const COMMAND_PLAY = "PLAY";
+export const COMMAND_PAUSE = "PAUSE";
+export const COMMAND_SEEK_TO = "SEEK_TO";
+export const COMMAND_STOP = "STOP";
+export const COMMAND_VOLUME = "VOLUME";
